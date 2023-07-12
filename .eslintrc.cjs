89caad1e1a46f124590bebb5aaa4798bbbbcf30d
achmadandy10 <mrtevos17@gmail.com>
2023-07-12 18:47:20 +0700

    chore: add config, eslint, prettier, and husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react/no-children-prop': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'import/extensions': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/indent': 'off',
+  },
+};
